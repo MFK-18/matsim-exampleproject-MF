df45457d330f8b7758915004e031290c9ee098e2
Mike <mike_richi@hotmail.de>
2020-05-04 18:53:51 +0200

    Removed Agents and added one with a simple plan

--- src/main/java/org/matsim/project/RunMatsim2.java
+++ src/main/java/org/matsim/project/RunMatsim2.java
@@ -22,7 +22,6 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.population.*;
-import org.matsim.contrib.otfvis.OTFVisLiveModule;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
@@ -32,13 +31,11 @@ import org.matsim.core.scenario.ScenarioUtils;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.matsim.api.core.v01.Id.createPersonId;
-
 /**
  * @author nagel
  *
  */
-public class RunMatsim{
+public class RunMatsim2 {
 
 	public static void main(String[] args) {
 
@@ -59,45 +56,6 @@ public class RunMatsim{
 		Scenario scenario = ScenarioUtils.loadScenario(config) ;
 		
 		// possibly modify scenario here
-		Id<Person> interestingPersonId = Id.createPersonId(1);
-		List<Id<Person>> personToRemove = new ArrayList<>();
-
-		for (Id<Person> personId : scenario.getPopulation().getPersons().keySet()){
-			if (!personId.equals(interestingPersonId)) {
-				personToRemove.add(personId);
-			}
-
-		}
-
-		for (Id<Person> personId : personToRemove) {
-			scenario.getPopulation().removePerson(personId);
-		}
-
-		System.out.println("Population Size = " + scenario.getPopulation().getPersons().size());
-		// ---
-
-		PopulationFactory populationFactory = scenario.getPopulation().getFactory();
-
-		Person person2 = populationFactory.createPerson(Id.createPersonId("Mike"));
-
-		Plan plan = populationFactory.createPlan();
-
-		Activity homeActivity = populationFactory.createActivityFromLinkId("h", Id.createLinkId(21));
-		homeActivity.setEndTime(6*60*60);
-		plan.addActivity(homeActivity);
-
-		Leg leg = populationFactory.createLeg(TransportMode.car);
-		plan.addLeg(leg);
-
-		Activity workActivity = populationFactory.createActivityFromLinkId("w", Id.createLinkId(1));
-		plan.addActivity(workActivity);
-
-		person2.addPlan(plan);
-
-		scenario.getPopulation().addPerson(person2);
-
-		System.out.println("Population Size = " + scenario.getPopulation().getPersons().size());
-
 
 		//
 
@@ -106,7 +64,7 @@ public class RunMatsim{
 		
 		// possibly modify controler here
 
-		controler.addOverridingModule( new OTFVisLiveModule() ) ;
+//		controler.addOverridingModule( new OTFVisLiveModule() ) ;
 		
 		// ---
 		
